55dc3c47861a9b84450d5506cb4acfb3fccf1080
g.nikolaev <mendeleev8@yandex.ru>
2026-08-14 17:54:05 +0300

    1 игра работает

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,19 +1,20 @@
 import random
 import prompt
 
-i = 0
-print('Answer "yes" if the number is even, otherwise answer "no".')
-while i < 3:
-    answer = ''
-    rundom_num = random.randint(1, 100)
-    if rundom_num % 2 == 0:
-        answer += 'yes'
-    else:
-        answer += 'no'
-    ask_num = print('Question: ' + str(rundom_num))
-    answer_number = prompt.string('Your answer: ')
-    if answer_number == answer:
-        print('Correct!')
-    else:
-        print(f'{answer_number} is wrong answer ;(. Correct answer was {answer}')
-    i+=1
+def even_or_odd():
+    i = 0
+    print('Answer "yes" if the number is even, otherwise answer "no".')
+    while i < 3:
+        answer = ''
+        rundom_num = random.randint(1, 100)
+        if rundom_num % 2 == 0:
+            answer += 'yes'
+        else:
+            answer += 'no'
+        ask_num = print('Question: ' + str(rundom_num))
+        answer_number = prompt.string('Your answer: ')
+        if answer_number == answer:
+            print('Correct!')
+        else:
+            print(f'{answer_number} is wrong answer ;(. Correct answer was {answer}')
+        i+=1
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,9 +1,10 @@
 from brain_games import cli
-
+from . import brain_even
 
 def main():
     print("Welcome to the Brain Games!")
     cli.welcome_user()
+    brain_even.even_or_odd()
 
 
 if __name__ == "__main__":
