d1c9719e8766322ba50a4319a034af09bde73465
g.nikolaev <mendeleev8@yandex.ru>
2026-08-17 20:58:59 +0300

    brain_calc

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -2,11 +2,33 @@ from . import brain_games
 import prompt
 import random
 
-def main():
-    brain_games.greet()
+def calc():
+    name = brain_games.greet()
     print('What is the result of the expression?')
     i = 0
     while i < 3:
         number1 = random.randint(1, 100)
         number2 = random.randint(1, 100)
-        expression = ()
+        expressions = ['+', '-', '*']
+        expression = random.choice(expressions)
+        question = print(f'Question: {number1} {expression} {number2}')
+        result = 0
+        if expression == '+':
+            result = number1 + number2
+        elif expression == '-':
+            result = number1 - number2
+        elif expression == '*':
+            result = number1 * number2
+        answer = prompt.string('Your answer: ')
+        if answer == str(result):
+            print('Correct!')
+            i += 1
+        else:
+            print(f"'{answer}' is wrong answer ;(. Correct answer was '{result}'\nLet's try again, {name}!")
+            break
+
+def main():
+    calc()
+
+if __name__ == '__main__':
+    main()
